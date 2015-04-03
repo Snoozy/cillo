a9203ec44203ee610e1b5bbb34bf38ad974e77ab
Daniel <danielli803@gmail.com>
2015-04-03 11:15:58 -0700

    Starting board settings page.

--- play/app/com/cillo/core/web/controllers/SettingsController.scala
+++ play/app/com/cillo/core/web/controllers/SettingsController.scala
@@ -8,14 +8,69 @@ import play.api.mvc._
 
 object SettingsController extends Controller {
 
-    def settingsPage = AuthAction { implicit user => implicit request =>
+    def userSettingsPage = AuthAction { implicit user => implicit request =>
         user match {
             case None => Found("/login")
             case Some(_) => Ok(com.cillo.core.web.views.html.core.settings(user.get))
         }
     }
 
-    def settingsChange = AuthAction { implicit user => implicit request =>
+    def boardSettingsPage(name: String) = AuthAction { implicit user => implicit request =>
+        user match {
+            case None => Found("/login")
+            case Some(_) =>
+                val board = Board.find(name)
+                if (board.isDefined) {
+                    if (board.get.creator_id == user.get.user_id.get) {
+                        Ok(com.cillo.core.web.views.html.core.board_settings(user.get, board.get))
+                    } else {
+                        NotFound("Permission denied.")
+                    }
+                } else {
+                    NotFound("Board not found.")
+                }
+        }
+    }
+
+    def boardSettingsChange(name: String) = AuthAction { implicit user => implicit request =>
+        user match {
+            case None => Found("/login")
+            case Some(_) =>
+                val board = Board.find(name)
+                if (board.isDefined) {
+                    val body = request.body.asMultipartFormData
+                    if (body.isDefined) {
+                        val picID: Int = {
+                            val file = body.get.file("picture")
+                            if (file.isDefined) {
+                                val pic = file.get.ref.file
+                                if (pic.length() < 3145728) {
+                                    val id = S3.upload(pic)
+                                    if (id.isDefined) {
+                                        val mediaID = Media.create(0, id.get)
+                                        if (mediaID.isDefined) {
+                                            mediaID.get.toInt
+                                        } else
+                                            user.get.photo_id
+                                    } else
+                                        user.get.photo_id
+                                } else
+                                    user.get.photo_id
+                            } else
+                                user.get.photo_id
+                        }
+                        val res = body.get.asFormUrlEncoded
+                        val desc = res.get("desc").map(_.head).getOrElse(user.get.name)
+                        Board.update(board.get.board_id.get, desc, picID)
+                    }
+                    Found("/" + board.get.name + "/settings")
+                } else {
+                    NotFound("Board not found.")
+                }
+        }
+    }
+
+    def userSettingsChange = AuthAction { implicit user => implicit request =>
         user match {
             case None => Found("/login")
             case Some(_) =>
