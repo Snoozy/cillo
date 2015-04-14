4c8e8334cbfd65b4a7bc1ccf53fb3572db3410a8
Daniel <danielli803@gmail.com>
2015-04-14 03:26:24 -0700

    Fucking deleted the deploy files....had to recreate...

---
+++ play/deploy-api.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo 'Generating package...'
+
+pack_path=$(activator dist | tee /dev/tty | awk '/package is ready/ {print $7;}')
+
+package_path=${pack_path%????}
+
+fullfile=$(basename ${package_path})
+
+filename=${fullfile%????}
+
+echo "Package is located at ${package_path}"
+
+server_ips="54.69.177.193"
+
+echo 'Starting deployment...'
+
+if [ -e "$package_path" ]; then
+    for server in "$server_ips"
+    do
+        echo "Deploying to ${server}."
+        scp "$package_path" ubuntu@${server}:/home/ubuntu/cillo-api.zip
+        rsync -avz $HOME/.cillo/prod_api.conf ubuntu@${server}:/home/ubuntu/
+        ssh ubuntu@${server} bash -c "'
+            unzip -o cillo-api.zip
+            sudo rm -rf cillo-backup/
+            mv cillo/ cillo-backup/
+            mv ${filename}/ cillo/
+            chmod 755 ./cillo/bin/cillo
+            rm cillo-api.zip
+        '"
+        ssh ubuntu@${server} "eval sudo kill \$(ps aux | grep [c]illo | awk '{print \$2}')"
+        ssh ubuntu@${server} bash -c "'
+            sudo ./cillo/bin/cillo -J-Xms128M -J-Xmx512m -J-server -Dconfig.file=/home/ubuntu/prod_api.conf -Dhttp.port=80 &
+            disown
+        '"
+    done
+else
+    echo 'Deployment error.'
+fi
