9443c08d712c4199da0f20f8e07ba8101641db57
Daniel <danielli803@gmail.com>
2015-12-27 00:08:39 -0500

    Tweaking colors and stuff

--- play/app/com/cillo/core/CilloConfig.scala
+++ play/app/com/cillo/core/CilloConfig.scala
@@ -3,10 +3,10 @@ package com.cillo.core
 object CilloConfig {
 
     // REDIRECTION CONFIG
-    final val RedirectHttp = false
-    final val RedirectMobile = false
+    final val RedirectHttp = true
+    final val RedirectMobile = true
     // should redirect cillo.co to www.cillo.co
-    final val RedirectToWWW = false
+    final val RedirectToWWW = true
 
     // ETC CONFIG
     // should be responding correctly to elb health checks
